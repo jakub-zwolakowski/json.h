d4342175e6bbc3cedf85bdad5794319e7942aee4
Jakub Zwolakowski <jakub.zwolakowski@trust-in-soft.com>
2021-01-21 10:52:00 +0100

    CLEAN-UP stub.c

--- trustinsoft/stub.c
+++ trustinsoft/stub.c
@@ -1,12 +1,11 @@
-
-int isatty(int fd) {
-  return 0;
-}
-
 #include <time.h>
 
 int clock_gettime(clockid_t clk_id, struct timespec *tp) {
   tp->tv_sec = 42;
   tp->tv_nsec = 42;
+  return 0;
+}
+
+int isatty(int fd) {
   return 0;
 }
